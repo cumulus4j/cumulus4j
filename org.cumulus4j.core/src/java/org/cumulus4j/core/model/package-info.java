1fd461c66a374dda2038ea18bd2728713b873f52
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-03-16 13:08:29 +0000

    More javadoc.

--- org.cumulus4j.core/src/java/org/cumulus4j/core/model/package-info.java
+++ org.cumulus4j.core/src/java/org/cumulus4j/core/model/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Data model classes used to store data and meta-data in the backend. Some of the classes in this package are
  * persistent and some are non-persistent. The persistent classes are managed by a
- * backend-JDO-{@link javax.jdo.PersistenceManager}.
+ * backend-{@link javax.jdo.PersistenceManager}.
  */
 package org.cumulus4j.core.model;
