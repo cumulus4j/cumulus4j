9f62f00535d5d542da03a98352f8d09bfd3e5c64
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-03-17 12:44:15 +0000

    Refactored Account tests.

---
+++ org.cumulus4j.test/src/test/org/cumulus4j/test/account/AccountInsertTest.java
@@ -0,0 +1,50 @@
+package org.cumulus4j.test.account;
+
+import org.cumulus4j.test.account.id.AnchorID;
+import org.cumulus4j.test.account.id.LocalAccountantDelegateID;
+import org.cumulus4j.test.framework.AbstractTransactionalTest;
+import org.cumulus4j.test.framework.CleanupUtil;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AccountInsertTest
+extends AbstractTransactionalTest
+{
+	private static final Logger logger = LoggerFactory.getLogger(AccountInsertTest.class);
+
+	private static final String ORGANISATION_ID = "jfire.my.org";
+	private static final LocalAccountantDelegateID LOCAL_ACCOUNTANT_DELEGATE_ID_0 = LocalAccountantDelegateID.create(ORGANISATION_ID, "0");
+	private static final AnchorID ACCOUNT_ID_0 = AnchorID.create(ORGANISATION_ID, Account.ANCHOR_TYPE_ID_ACCOUNT, "voucher.00");
+
+	@BeforeClass
+	public static void clearDatabase()
+	throws Exception
+	{
+		logger.info("clearDatabase: Clearing database (dropping all tables).");
+		CleanupUtil.dropAllTables();
+	}
+
+	@Test
+	public void createData()
+	{
+		{
+			Account account = new Account(ACCOUNT_ID_0);
+			LocalAccountantDelegate localAccountantDelegate = new LocalAccountantDelegate(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
+			localAccountantDelegate.setAccount("EUR", account);
+			pm.makePersistent(localAccountantDelegate); // this should implicitely persist the account
+		}
+
+		pm.currentTransaction().commit();
+		pm.currentTransaction().begin();
+
+		{
+			LocalAccountantDelegate localAccountantDelegate = (LocalAccountantDelegate) pm.getObjectById(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
+			localAccountantDelegate.test();
+
+			Account account = (Account) pm.getObjectById(ACCOUNT_ID_0);
+			account.getBalance();
+		}
+	}
+}
--- org.cumulus4j.test/src/test/org/cumulus4j/test/account/AccountTest.java
+++ org.cumulus4j.test/src/test/org/cumulus4j/test/account/AccountTest.java
@@ -9,6 +9,9 @@ import org.cumulus4j.test.account.id.AnchorID;
 import org.cumulus4j.test.account.id.LocalAccountantDelegateID;
 import org.cumulus4j.test.framework.AbstractTransactionalTest;
 import org.cumulus4j.test.framework.CleanupUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -32,82 +35,74 @@ extends AbstractTransactionalTest
 		CleanupUtil.dropAllTables();
 	}
 
-	@Test
+	@Before
 	public void createData()
 	{
 		Account account = new Account(ACCOUNT_ID_0);
 		LocalAccountantDelegate localAccountantDelegate = new LocalAccountantDelegate(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
 		localAccountantDelegate.setAccount("EUR", account);
 		pm.makePersistent(localAccountantDelegate); // this should implicitely persist the account
-	}
 
-	@Test
-	public void getObjectById() throws IOException
-	{
-		LocalAccountantDelegate localAccountantDelegate = (LocalAccountantDelegate) pm.getObjectById(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
-		localAccountantDelegate.test();
-
-		Account account = (Account) pm.getObjectById(ACCOUNT_ID_0);
-		account.getBalance();
-	}
-
-	@Test
-	public void updateData0() throws IOException
-	{
-		LocalAccountantDelegate localAccountantDelegate = (LocalAccountantDelegate) pm.getObjectById(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
-		localAccountantDelegate.setName("Test 0000");
-		localAccountantDelegate.setDescription(
-				"This is a very long description bla bla bla trallalala tröt tröt. And " +
-				"I don't know exactly what I should write here, bla bla bla, but it should " +
-				"be really really long! Very likely this is sufficient now, but I'd better " +
-				"add some more words.\n\n" +
-				"\n" +
-				"Freude, schöner Götterfunken,\n" +
-				"Tochter aus Elisium,\n" +
-				"Wir betreten feuertrunken\n" +
-				"Himmlische, dein Heiligthum.\n" +
-				"Deine Zauber binden wieder,\n" +
-				"was der Mode Schwerd getheilt;\n" +
-				"Bettler werden Fürstenbrüder,\n" +
-				"wo dein sanfter Flügel weilt.\n" +
-				"\n" +
-				"Seid umschlungen, Millionen!\n" +
-				"Diesen Kuß der ganzen Welt!\n" +
-				"Brüder – überm Sternenzelt\n" +
-				"muß ein lieber Vater wohnen.\n" +
-				"\n" +
-				"Wem der große Wurf gelungen,\n" +
-				"eines Freundes Freund zu seyn;\n" +
-				"wer ein holdes Weib errungen,\n" +
-				"mische seinen Jubel ein!\n" +
-				"Ja – wer auch nur eine Seele\n" +
-				"sein nennt auf dem Erdenrund!\n" +
-				"Und wer’s nie gekonnt, der stehle\n" +
-				"weinend sich aus diesem Bund!\n"
-		);
-	}
-
-	@Test
-	public void updateData1() throws IOException
-	{
-		LocalAccountantDelegate localAccountantDelegate = (LocalAccountantDelegate) pm.getObjectById(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
 		localAccountantDelegate.setAccount("CHF", new Account(ACCOUNT_ID_1));
 		localAccountantDelegate.setName("New test bla bla bla.");
 		localAccountantDelegate.setDescription("description");
 	}
 
 	@Test
-	public void query0() throws IOException
+	public void updateData() throws IOException
+	{
+		String name = "Test 0000";
+		String description = "This is a very long description bla bla bla trallalala tröt tröt. And " +
+		"I don't know exactly what I should write here, bla bla bla, but it should " +
+		"be really really long! Very likely this is sufficient now, but I'd better " +
+		"add some more words.\n\n" +
+		"\n" +
+		"Freude, schöner Götterfunken,\n" +
+		"Tochter aus Elisium,\n" +
+		"Wir betreten feuertrunken\n" +
+		"Himmlische, dein Heiligthum.\n" +
+		"Deine Zauber binden wieder,\n" +
+		"was der Mode Schwerd getheilt;\n" +
+		"Bettler werden Fürstenbrüder,\n" +
+		"wo dein sanfter Flügel weilt.\n" +
+		"\n" +
+		"Seid umschlungen, Millionen!\n" +
+		"Diesen Kuß der ganzen Welt!\n" +
+		"Brüder – überm Sternenzelt\n" +
+		"muß ein lieber Vater wohnen.\n" +
+		"\n" +
+		"Wem der große Wurf gelungen,\n" +
+		"eines Freundes Freund zu seyn;\n" +
+		"wer ein holdes Weib errungen,\n" +
+		"mische seinen Jubel ein!\n" +
+		"Ja – wer auch nur eine Seele\n" +
+		"sein nennt auf dem Erdenrund!\n" +
+		"Und wer’s nie gekonnt, der stehle\n" +
+		"weinend sich aus diesem Bund!\n";
+
+		LocalAccountantDelegate localAccountantDelegate = (LocalAccountantDelegate) pm.getObjectById(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
+		localAccountantDelegate.setName(name);
+		localAccountantDelegate.setDescription(description);
+
+		pm.currentTransaction().commit();
+		pm.currentTransaction().begin();
+
+		localAccountantDelegate = (LocalAccountantDelegate) pm.getObjectById(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
+		Assert.assertEquals(name, localAccountantDelegate.getName());
+		Assert.assertEquals(description, localAccountantDelegate.getDescription());
+	}
+
+	@Test
+	public void extentIterator() throws IOException
 	{
 		LocalAccountantDelegate localAccountantDelegate = pm.getExtent(LocalAccountantDelegate.class).iterator().next();
 		localAccountantDelegate.test();
 	}
 
 	@Test
-	public void query1() throws IOException
+	public void queryStringEquals() throws IOException
 	{
 		Query q = pm.newQuery(LocalAccountantDelegate.class);
-//		q.setFilter("this.name.indexOf(:needle) >= 0");
 		q.setFilter("this.name == :name");
 
 		@SuppressWarnings("unchecked")
@@ -127,7 +122,7 @@ extends AbstractTransactionalTest
 	}
 
 	@Test
-	public void query2() throws IOException
+	public void queryAndWithTwoStringEquals() throws IOException
 	{
 		Query q = pm.newQuery(LocalAccountantDelegate.class);
 		q.setFilter("this.name == :pName && this.description == :pDesc");
@@ -151,7 +146,7 @@ extends AbstractTransactionalTest
 	}
 
 	@Test
-	public void query3() throws IOException
+	public void queryStringIndexOf() throws IOException
 	{
 		Query q = pm.newQuery(LocalAccountantDelegate.class);
 		q.setFilter("this.name.indexOf(:needle) >= 0");
@@ -172,7 +167,7 @@ extends AbstractTransactionalTest
 		System.out.println();
 	}
 
-	@Test
+	@After
 	public void deleteAll() throws IOException
 	{
 		LocalAccountantDelegate localAccountantDelegate = (LocalAccountantDelegate) pm.getObjectById(LOCAL_ACCOUNTANT_DELEGATE_ID_0);
