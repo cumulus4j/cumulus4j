f14a08b3a3e530ef018d816843b805cd7a881ccc
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-04-04 17:13:04 +0000

    Using 'org.apache.felix:maven-bundle-plugin' to generate OSGi descriptors.

---
+++ org.cumulus4j.api/src/java/org/cumulus4j/api/annotation/Dummy.java
@@ -0,0 +1,15 @@
+package org.cumulus4j.api.annotation;
+
+/**
+ * <p>
+ * Currently exists only to test Maven build configuration!
+ * </p>
+ * <p>
+ * TODO Delete when this package contains real annotations!
+ * </p>
+ *
+ * @author Marco หงุ่ยตระกูล-Schulze - marco at nightlabs dot de
+ */
+public @interface Dummy {
+
+}
