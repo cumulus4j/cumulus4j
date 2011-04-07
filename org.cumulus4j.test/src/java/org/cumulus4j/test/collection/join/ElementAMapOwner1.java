cd3dacefac8b793f51884fb4b677c36237e5b135
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-04-07 08:13:13 +0000

    Added new persistable classes for @Join Map tests. https://sourceforge.net/tracker/?func=detail&aid=3277875&group_id=517465&atid=2102914

---
+++ org.cumulus4j.test/src/java/org/cumulus4j/test/collection/join/ElementAMapOwner1.java
@@ -0,0 +1,35 @@
+package org.cumulus4j.test.collection.join;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jdo.annotations.Join;
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable
+public class ElementAMapOwner1
+{
+	private String name;
+
+	@Join
+	private Map<String, ElementA> map = new HashMap<String, ElementA>();
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Map<String, ElementA> getMap() {
+		return map == null ? null : Collections.unmodifiableMap(map);
+	}
+	public void addElementA(String key, ElementA elementA)
+	{
+		map.put(key, elementA);
+	}
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + "[name=\"" + name + "\"]";
+	}
+}
