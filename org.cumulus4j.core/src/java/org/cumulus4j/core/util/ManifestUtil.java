bc168c8c5b25c66673e22344dd9f778792a9de45
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-04-07 12:33:15 +0000

    Fixed version detection in JARs.

--- org.cumulus4j.core/src/java/org/cumulus4j/core/util/ManifestUtil.java
+++ org.cumulus4j.core/src/java/org/cumulus4j/core/util/ManifestUtil.java
@@ -1,13 +1,17 @@
 package org.cumulus4j.core.util;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.jar.Manifest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ManifestUtil
 {
+	private static final Logger logger = LoggerFactory.getLogger(ManifestUtil.class);
+
 	public static Manifest readManifest(Class<?> clazz)
 	throws IOException
 	{
@@ -18,30 +22,14 @@ public class ManifestUtil
 
 		String referenceClassRelativePath = "/" + clazz.getName().replace('.', '/') + ".class";
 		URL referenceClassURL = clazz.getResource(referenceClassRelativePath);
+		logger.trace("readManifest: referenceClassURL={}", referenceClassURL);
+
 		String referenceClassURLBase = referenceClassURL.toExternalForm();
 		if (!referenceClassURLBase.endsWith(referenceClassRelativePath))
 			throw new IllegalStateException("referenceClassURL does not end on \"" + referenceClassRelativePath + "\": " + referenceClassURLBase);
 
 		referenceClassURLBase = referenceClassURLBase.substring(0, referenceClassURLBase.length() - referenceClassRelativePath.length());
-
-		// strip the EJB-JAR-inside part of the URL ("jar:" at the beginning and "!" at the end)
-		String jarPrefix = "jar:";
-		if (referenceClassURLBase.startsWith(jarPrefix)) {
-			if (!referenceClassURLBase.endsWith("!"))
-				throw new IllegalStateException("referenceClassURLBase starts with \"" + jarPrefix + "\" but does not end with \"!\": " + referenceClassURLBase);
-
-			referenceClassURLBase = referenceClassURLBase.substring(jarPrefix.length(), referenceClassURLBase.length() - 1);
-
-			// strip the name of the JAR at the end
-			if (referenceClassURLBase.endsWith("/") || referenceClassURLBase.endsWith(File.separator))
-				throw new IllegalStateException("Expected a normal character but found \"/\" at the end: " + referenceClassURLBase);
-
-			int lastSlashIdx = referenceClassURLBase.replace(File.separatorChar, '/').lastIndexOf('/'); // We find both, slashes and backslashes this way.
-			if (lastSlashIdx < 0)
-				throw new IllegalStateException("referenceClassURLBase does not contain any EAR! Cannot find separator anymore: " + referenceClassURLBase);
-
-			referenceClassURLBase = referenceClassURLBase.substring(0, lastSlashIdx);
-		}
+		logger.trace("readManifest: referenceClassURLBase={}", referenceClassURLBase);
 
 		String manifestResourceName = "/META-INF/MANIFEST.MF";
 		URL manifestResourceUrl = new URL(referenceClassURLBase + manifestResourceName);
