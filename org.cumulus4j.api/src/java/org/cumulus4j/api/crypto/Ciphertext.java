004f5c6916937c54a7a28d242826fe6b95008d6d
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-04-13 09:52:51 +0000

    More javadoc.

--- org.cumulus4j.api/src/java/org/cumulus4j/api/crypto/Ciphertext.java
+++ org.cumulus4j.api/src/java/org/cumulus4j/api/crypto/Ciphertext.java
@@ -1,6 +1,7 @@
 package org.cumulus4j.api.crypto;
 
 /**
+ * Object representing encrypted information (aka <a href="http://en.wikipedia.org/wiki/Ciphertext">ciphertext</a>).
  *
  * @author Marco หงุ่ยตระกูล-Schulze - marco at nightlabs dot de
  */
@@ -8,18 +9,35 @@ public class Ciphertext
 {
 	private long keyID = -1;
 
+	/**
+	 * Get the identifier of the key used to encrypt the {@link #getData() data}.
+	 *
+	 * @return the key identifier.
+	 */
 	public long getKeyID() {
 		return keyID;
 	}
+	/**
+	 * Set the identifier of the key used to encrypt the {@link #getData() data}.
+	 * @param keyID the key identifier.
+	 */
 	public void setKeyID(long keyID) {
 		this.keyID = keyID;
 	}
 
 	private byte[] data;
 
+	/**
+	 * Get the encrypted information.
+	 * @return the encrypted information or <code>null</code>.
+	 */
 	public byte[] getData() {
 		return data;
 	}
+	/**
+	 * Set the encrypted information.
+	 * @param data the encrypted information or <code>null</code>.
+	 */
 	public void setData(byte[] data) {
 		this.data = data;
 	}
--- org.cumulus4j.api/src/java/org/cumulus4j/api/crypto/Plaintext.java
+++ org.cumulus4j.api/src/java/org/cumulus4j/api/crypto/Plaintext.java
@@ -1,6 +1,7 @@
 package org.cumulus4j.api.crypto;
 
 /**
+ * Object representing unencrypted information (aka <a href="http://en.wikipedia.org/wiki/Plaintext">plaintext</a>).
  *
  * @author Marco หงุ่ยตระกูล-Schulze - marco at nightlabs dot de
  */
@@ -8,9 +9,17 @@ public class Plaintext
 {
 	private byte[] data;
 
+	/**
+	 * Get the unencrypted data.
+	 * @return the unencrypted data or <code>null</code>.
+	 */
 	public byte[] getData() {
 		return data;
 	}
+	/**
+	 * Set the unencrypted data.
+	 * @param data the unencrypted data or <code>null</code>.
+	 */
 	public void setData(byte[] data) {
 		this.data = data;
 	}
