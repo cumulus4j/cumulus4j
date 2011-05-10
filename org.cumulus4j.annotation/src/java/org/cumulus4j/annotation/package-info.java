2d446c2cbb5a7e06fecbac859231ca4c8cc827e9
Andy Jefferson <andy@datanucleus.org>
2011-05-10 13:05:07 +0000

    javadoc

---
+++ org.cumulus4j.annotation/src/java/org/cumulus4j/annotation/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>
+ * This package provides annotations to enhance the specification capabilities of Cumulus4j, defining which fields
+ * are queryable (and hence indexed) and which aren't for example. You can typically avoid use of these
+ * annotations by using the standard JDO @Extension annotation, but they are here since could be considered
+ * more user-friendly (with the downside of imposing an extra dependency).
+ * </p>
+ */
+package org.cumulus4j.annotation;
