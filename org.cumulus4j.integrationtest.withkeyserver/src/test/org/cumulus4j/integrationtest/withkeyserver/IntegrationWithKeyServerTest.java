e6fe7a10cc53b4ac248c040746ae7ab74e02753c
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-05-11 17:51:09 +0000

    Trying to get the integration-test simulating the 3-computer-scenario (client, app-server, key-server) running.

---
+++ org.cumulus4j.integrationtest.withkeyserver/src/test/org/cumulus4j/integrationtest/withkeyserver/IntegrationWithKeyServerTest.java
@@ -0,0 +1,13 @@
+package org.cumulus4j.integrationtest.withkeyserver;
+
+import org.junit.Test;
+
+public class IntegrationWithKeyServerTest
+{
+	@Test
+	public void testThreeComputerConfiguration()
+	throws Exception
+	{
+		Thread.sleep(30000);
+	}
+}
