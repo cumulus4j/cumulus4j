a51bd3e394550dc5f974a7726d4ee660d61f9b93
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-05-11 15:50:40 +0000

    Removed TODO tag.

--- org.cumulus4j.store/src/java/org/cumulus4j/store/annotation/datanucleus/package-info.java
+++ org.cumulus4j.store/src/java/org/cumulus4j/store/annotation/datanucleus/package-info.java
@@ -2,19 +2,5 @@
  * <p>
  * This package provides handlers for use of the Cumulus4J annotations with DataNucleus.
  * </p>
- * TODO This should IMHO be moved into org.cumulus4j.store (or another, not yet existing project), because
- * it causes org.cumulus4j.annotation to have a dependency onto org.datanucleus.core. The negative effect is that
- * a client program not using DataNucleus itself would require to have DN deployed when it has a dependency on data
- * model classes (which is likely, if no DTOs are used). Data model classes should only require a dependency on
- * javax.jdo (or javax.persistence) and - if cumulus4j annotations are used - on org.cumulus4j.annotation. There should
- * be no further dependencies required.
- *
- * We can discuss this further, if there are reasons which I overlooked. But at the moment, I'm convinced, we should
- * remove the dependency "org.cumulus4j.annotation => org.datanucleus.core".
- *
- * org.cumulus4j.store has a dependency onto org.datanucleus.core already, anyway. Hence, a new package there named
- * org.cumulus4j.store.annotationhandler or org.cumulus4j.store.annotation.datanucleus or similar would IMHO make sense.
- *
- * Marco :-)
  */
 package org.cumulus4j.store.annotation.datanucleus;
