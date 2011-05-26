26112071d8ee5c7149c50ad5eb8f8b5d2d0639f1
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-05-26 15:38:32 +0000

    More javadoc.

---
+++ org.cumulus4j.store.crypto.keymanager/src/java/org/cumulus4j/store/crypto/keymanager/messagebroker/pmf/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * {@link javax.jdo.PersistenceManagerFactory}-backed implementation of {@link org.cumulus4j.store.crypto.keymanager.messagebroker.MessageBroker MessageBroker}.
+ */
+package org.cumulus4j.store.crypto.keymanager.messagebroker.pmf;
