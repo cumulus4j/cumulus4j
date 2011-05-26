d27b13c08dacbf94f64209aa9815a74e6803fa2a
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-05-26 15:15:18 +0000

    More javadoc.

---
+++ org.cumulus4j.store.crypto.keymanager/src/java/org/cumulus4j/store/crypto/keymanager/rest/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>
+ * REST API for the communication between key-manager and application-server.
+ * </p>
+ * <p>
+ * As documented in <a href="http://cumulus4j.org/documentation/deployment-scenarios.html">Deployment scenarios</a>,
+ * TCP connections are always established from the key-manager (i.e. client or key-server) to the application server.
+ * The REST API provided by this package is the yellow + red arrows in the graphics of the 'Deployment scenarios'.
+ * </p>
+ */
+package org.cumulus4j.store.crypto.keymanager.rest;
