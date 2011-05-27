e8b4e6b1be58279c0ade1c402012f80e64099a5b
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-05-27 21:34:01 +0000

    Created new reference keystore (optimized property storage in my previous check-in).

--- org.cumulus4j.keystore/src/test/org/cumulus4j/keystore/test/KeyStoreCompatibilityTest.java
+++ org.cumulus4j.keystore/src/test/org/cumulus4j/keystore/test/KeyStoreCompatibilityTest.java
@@ -6,6 +6,8 @@ import java.security.Key;
 import junit.framework.Assert;
 
 import org.cumulus4j.keystore.KeyStore;
+import org.cumulus4j.keystore.prop.LongProperty;
+import org.cumulus4j.keystore.prop.StringProperty;
 import org.cumulus4j.keystore.test.resource.ResourceHelper;
 import org.junit.Test;
 import org.nightlabs.util.IOUtil;
@@ -20,11 +22,63 @@ public class KeyStoreCompatibilityTest
 	protected static final char[] PASSWORD = KeyStoreKeyTest.PASSWORD;
 
 	@Test
+	public void createNewReferenceKeyStore()
+	throws Exception
+	{
+		File keyStoreFile = File.createTempFile("new-reference-", ".keystore");
+		KeyStore keyStore = new KeyStore(keyStoreFile);
+
+		keyStore.createUser(null, null, USER, PASSWORD);
+		keyStore.createUser(USER, PASSWORD, "eddie", "da-pass".toCharArray());
+		keyStore.createUser(USER, PASSWORD, "berny", "mai sikret".toCharArray());
+
+		// We populate the key store with all things that are supported.
+		// This should be kept complete when adding new Property implementations!
+		keyStore.generateKey(USER, PASSWORD);
+		keyStore.generateKey(USER, PASSWORD);
+		keyStore.generateKeys(USER, PASSWORD, 1000);
+
+		{
+			LongProperty p = keyStore.getProperty(USER, PASSWORD, LongProperty.class, "long1");
+			p.setValue(12345L);
+			keyStore.setProperty(USER, PASSWORD, p);
+		}
+
+		{
+			LongProperty p = keyStore.getProperty(USER, PASSWORD, LongProperty.class, "long2");
+			p.setValue(-5557347874L);
+			keyStore.setProperty(USER, PASSWORD, p);
+		}
+
+		{
+			StringProperty p = keyStore.getProperty(USER, PASSWORD, StringProperty.class, "string1");
+			p.setValue(
+					"Freude, schöner Götterfunken,\n" +
+					"Tochter aus Elysium,\n" +
+					"Wir betreten feuertrunken,\n" +
+					"Himmlische, dein Heiligthum.\n"
+			);
+			keyStore.setProperty(USER, PASSWORD, p);
+		}
+
+		{
+			StringProperty p = keyStore.getProperty(USER, PASSWORD, StringProperty.class, "string2");
+			p.setValue(
+					"Freude, schöner Götterfunken,\n" +
+					"Tochter aus Elysium,\n" +
+					"Wir betreten feuertrunken,\n" +
+					"Himmlische, dein Heiligthum.\n"
+			);
+			keyStore.setProperty(USER, PASSWORD, p);
+		}
+	}
+
+	@Test
 	public void openReferenceKeyStore()
 	throws Exception
 	{
-		File keyStoreFile = File.createTempFile("test-", ".keystore");
-		IOUtil.copyResource(ResourceHelper.class, "test-1.keystore", keyStoreFile);
+		File keyStoreFile = File.createTempFile("current-reference-", ".keystore");
+		IOUtil.copyResource(ResourceHelper.class, "current-reference.keystore", keyStoreFile);
 		// The KeyStore reads the data immediately after creating a new instance.
 		KeyStore keyStore = new KeyStore(keyStoreFile);
 
