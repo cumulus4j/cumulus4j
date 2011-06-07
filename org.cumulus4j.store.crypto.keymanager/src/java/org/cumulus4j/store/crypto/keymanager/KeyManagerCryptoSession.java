c3cd383da4272577c0d45181f514814fc0e934e5
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-06-07 18:35:21 +0000

    Added javadoc for the data structure within the byte array.

--- org.cumulus4j.store.crypto.keymanager/src/java/org/cumulus4j/store/crypto/keymanager/KeyManagerCryptoSession.java
+++ org.cumulus4j.store.crypto.keymanager/src/java/org/cumulus4j/store/crypto/keymanager/KeyManagerCryptoSession.java
@@ -97,6 +97,47 @@ extends AbstractCryptoSession
 		return keyDecrypter;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * <p>
+	 * The implementation in {@link KeyManagerCryptoManager} stores every plaintext
+	 * encoded in the following form:
+	 * </p>
+	 * <table border="1" width="100%">
+	 * <tbody>
+	 * 	<tr>
+	 * 		<td><b>Bytes</b></td><td><b>Description</b></td>
+	 * 	</tr>
+	 * 	<tr>
+	 * 		<td align="right" valign="top">1</td><td>{@link EncryptionAlgorithm}'s ordinal value</td>
+	 * 	</tr><tr>
+	 * 		<td align="right" valign="top">1</td><td><i>ivLen</i>: Length of the IV in bytes.</td>
+	 * 	</tr><tr>
+	 * 		<td align="right" valign="top">ivLen</td><td>The actual IV (initialisation vector).</td>
+	 * 	</tr><tr>
+	 * 		<td colspan="2">
+	 * 			<table border="1" width="100%">
+	 * 			<tbody>
+	 * 				<tr>
+	 * 					<td colspan="2"><b>ENCRYPTED</b></td>
+	 * 				</tr><tr>
+	 * 					<td><b>Bytes</b></td><td><b>Description</b></td>
+	 * 				</tr><tr>
+	 *					<td align="right" valign="top">1</td><td>{@link ChecksumAlgorithm#toByte()}</td>
+	 *				</tr><tr>
+	 *					<td align="right" valign="top">1</td><td><i>checksumLen</i>: Length of the checksum in bytes.</td>
+	 *				</tr><tr>
+	 *					<td align="right" valign="top">checksumLen</td><td>The actual checksum.</td>
+	 *				</tr><tr>
+	 *					<td align="right" valign="top">all following</td><td>The actual data.</td>
+	 * 				</tr>
+	 * 			</tbody>
+	 * 			</table>
+	 * 		</td>
+	 * 	</tr>
+	 * </tbody>
+	 * </table>
+	 */
 	@Override
 	public Ciphertext encrypt(Plaintext plaintext)
 	{
@@ -284,143 +325,12 @@ extends AbstractCryptoSession
 
 	private ChecksumCalculator checksumCalculator = new ChecksumCalculator();
 
-//	private byte[] encrypt(Cipher encrypter, byte[] input) throws GeneralSecurityException
-//	{
-//		int saltLength = getSaltLength(encrypter);
-//		int resultSize = 1 /* encryptionAlgoID */ + encrypter.getOutputSize(saltLength + 1 /* checksum-algo-id */ + 4 /* checksum-length with CRC32 */ + input.length);
-//
-//		ByteBuffer resultBuf = ByteBuffer.allocate(resultSize);
-//		resultBuf.position(1); // We'll write this 'encryptionAlgoID' outside of this method after encrypting.
-//
-//		{ // First, we put salt into the soup ;-)
-//			// We start with the salt to make sure we have unpredictable plain-text already at the very beginning to
-//			// make attacks based on known plain-text impossible.
-//			byte[] salt = new byte[saltLength];
-//			random.nextBytes(salt);
-//			encrypter.update(ByteBuffer.wrap(salt), resultBuf);
-//		}
-//
-//		// Now we add the check-sum.
-//		// It always starts with the algo-identifier, followed by the checksum itself.
-//		switch (activeChecksumAlgorithm) {
-//			case none: {
-//				checksum = new byte[1];
-//				checksum[0] = (byte)activeChecksumAlgorithm.ordinal();
-//				break;
-//			}
-//			case crc32: {
-//				checksum = new byte[5]; // CRC32 has 32 bit, i.e. 4 bytes PLUS 1 byte for the checksumAlgoID
-//				checksum[0] = (byte)activeChecksumAlgorithm.ordinal();
-//				CRC32 crc32 = new CRC32();
-//				crc32.update(input);
-//				long crc32Value = crc32.getValue();
-//				for (int i = 1; i < checksum.length; ++i)
-//					checksum[i] = (byte)(crc32Value >>> ((i - 1) * 8));
-//
-//				break;
-//			}
-//			default:
-//				throw new IllegalStateException("Unsupported ChecksumAlgorithm: " + activeChecksumAlgorithm);
-//		}
-//		encrypter.update(ByteBuffer.wrap(checksum), resultBuf);
-//
-//		encrypter.doFinal(ByteBuffer.wrap(input), resultBuf);
-//		if (resultBuf.hasArray()) {
-//			if (resultBuf.array().length == resultBuf.position())
-//				return resultBuf.array();
-//			else
-//				logger.warn("Backing array cannot be directly used, because its size ({}) does not match the required size ({})!", resultBuf.array().length, resultBuf.position());
-//		}
-//		else
-//			logger.warn("Backing array cannot be directly used, because there is no backing array!");
-//
-//		byte[] result = new byte[resultBuf.position()];
-//		resultBuf.rewind();
-//		resultBuf.get(result);
-//		return result;
-//	}
-//
-//	private int getSaltLength(Cipher cipher)
-//	{
-//		int saltLength = cipher.getBlockSize();
-//		if (saltLength < 1)
-//			saltLength = 16;
-//
-//		return saltLength;
-//	}
-//
-//	private byte[] decrypt(Cipher decrypter, byte[] input) throws GeneralSecurityException, IOException
-//	{
-//		// Performance tests in CryptoAlgoBenchmark show that ByteBuffer is as fast as System.arraycopy(...) after
-//		// the JVM had time for hotspot-compilation (only at the beginning there are slight performance differences).
-//		// Hence we use the nicer API. Marco :-)
-//		ByteBuffer inputBuf = ByteBuffer.wrap(input);
-//
-//		// Skip 1st byte containing the encryptionAlgoID (handled outside of this method).
-//		inputBuf.position(1);
-//
-//		int rawPlainSize = decrypter.getOutputSize(input.length - inputBuf.position()); // '- inputBuf.position()' because of first skipped byte
-//		ByteBuffer rawBuf = ByteBuffer.allocate(rawPlainSize);
-//
-//		int bytesDecryptedCount = decrypter.doFinal(inputBuf, rawBuf);
-//
-//		// Discard the salt, the first byte we're interested in is the checksum-id AFTER the salt.
-//		int saltLength = getSaltLength(decrypter);
-//		rawBuf.position(saltLength);
-//
-//		int checksumAlgoID = rawBuf.get() & 0xff; // the '& 0xff' is necessary to use the whole UNSIGNED range of a byte, i.e. 0...255.
-//		if (checksumAlgoID > ChecksumAlgorithm.values().length - 1)
-//			throw new IllegalArgumentException("input[" + (rawBuf.position() - 1) + "] == checksumAlgoID == " + checksumAlgoID + " is unknown!");
-//
-//		ChecksumAlgorithm checksumAlgorithm = ChecksumAlgorithm.values()[checksumAlgoID];
-//		byte[] checksum;
-//		switch (checksumAlgorithm) {
-//			case none: {
-//				checksum = new byte[0];
-//				break;
-//			}
-//			case crc32: {
-//				checksum = new byte[4]; // CRC32 has 32 bit, i.e. 4 bytes
-////				System.arraycopy(raw, saltLength + 1, checksum, 0, checksum.length);
-//				rawBuf.get(checksum);
-//				break;
-//			}
-//			default:
-//				throw new IllegalStateException("Unsupported ChecksumAlgorithm: " + activeChecksumAlgorithm);
-//		}
-//
-////		byte[] result = new byte[raw.length - saltLength - 1 /* checksumAlgoID */ - checksum.length];
-////		System.arraycopy(raw, saltLength + 1 + checksum.length, result, 0, result.length);
-//		byte[] result = new byte[bytesDecryptedCount - rawBuf.position()];
-//		rawBuf.get(result);
-//
-//		// And finally calculate a new checksum and verify if it matches the one we read above.
-//		switch (checksumAlgorithm) {
-//			case none: // nothing to do
-//				break;
-//			case crc32: {
-//				CRC32 crc32 = new CRC32();
-//				crc32.update(result);
-//				long crc32Value = crc32.getValue();
-//				for (int i = 0; i < checksum.length; ++i) {
-//					if (checksum[i] != (byte)(crc32Value >>> (i * 8)))
-//						throw new IOException("CRC32 checksum mismatch!");
-//				}
-//
-//				break;
-//			}
-//			default:
-//				throw new IllegalStateException("Unsupported ChecksumAlgorithm: " + activeChecksumAlgorithm);
-//		}
-//
-//		return result;
-//	}
-
 	@Override
 	public void close() {
 		super.close();
 
-		// TODO clear caches.
+		// Our caches are used across multiple sessions for performance reasons,
+		// hence we cannot close the caches here.
 	}
 
 }
