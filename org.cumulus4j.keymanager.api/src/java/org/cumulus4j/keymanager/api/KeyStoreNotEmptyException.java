be125fed9873db9ecbc801f74087852a1401afe3
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-06-28 22:48:06 +0000

    Removed unnecessary TODO tags and added a bit javadoc.

--- org.cumulus4j.keymanager.api/src/java/org/cumulus4j/keymanager/api/KeyStoreNotEmptyException.java
+++ org.cumulus4j.keymanager.api/src/java/org/cumulus4j/keymanager/api/KeyStoreNotEmptyException.java
@@ -1,5 +1,13 @@
 package org.cumulus4j.keymanager.api;
 
+import org.cumulus4j.keystore.KeyStore;
+
+/**
+ * Thrown, if an {@link KeyStore#isEmpty() empty} <code>KeyStore</code> is expected, but the key-store is
+ * not empty. For example, some key-store-initialisations can only be done, if the key-store is empty (i.e. new).
+ *
+ * @author Marco หงุ่ยตระกูล-Schulze - marco at nightlabs dot de
+ */
 public class KeyStoreNotEmptyException extends KeyManagerException
 {
 	private static final long serialVersionUID = 1L;
--- org.cumulus4j.keystore/src/java/org/cumulus4j/keystore/KeyStoreNotEmptyException.java
+++ org.cumulus4j.keystore/src/java/org/cumulus4j/keystore/KeyStoreNotEmptyException.java
@@ -18,8 +18,8 @@
 package org.cumulus4j.keystore;
 
 /**
- * Thrown, if an {@link KeyStore#isEmpty() empty} KeyStore is expected, but the key-store is
- * not empty.
+ * Thrown, if an {@link KeyStore#isEmpty() empty} <code>KeyStore</code> is expected, but the key-store is
+ * not empty. For example, some key-store-initialisations can only be done, if the key-store is empty (i.e. new).
  *
  * @author Marco หงุ่ยตระกูล-Schulze - marco at nightlabs dot de
  */
@@ -27,23 +27,17 @@ public class KeyStoreNotEmptyException extends KeyStoreException
 {
 	private static final long serialVersionUID = 1L;
 
-	public KeyStoreNotEmptyException() {
-		// TODO Auto-generated constructor stub
-	}
+	public KeyStoreNotEmptyException() { }
 
 	public KeyStoreNotEmptyException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public KeyStoreNotEmptyException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public KeyStoreNotEmptyException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
-
 }
