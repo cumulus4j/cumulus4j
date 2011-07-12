163e9a60e55e62ea6dfc21c995d63eea56fa437b
Marco Nguitragool-Schulze <marco@nightlabs.de>
2011-07-12 22:57:28 +0000

    Replaced relative by absolute link (won't break when refactoring).

--- org.cumulus4j.keymanager.api/src/java/org/cumulus4j/keymanager/api/package-info.java
+++ org.cumulus4j.keymanager.api/src/java/org/cumulus4j/keymanager/api/package-info.java
@@ -4,7 +4,7 @@
  * </p><p>
  * The entry point into this API is the {@link org.cumulus4j.keymanager.api.KeyManagerAPI}. It provides a generic way
  * to manage a {@link org.cumulus4j.keystore.KeyStore} either in the local file system or on a remote key-server
- * (as shown in <a href="../../../../../documentation/deployment-scenarios.html">Deployment scenarios</a>).
+ * (as shown in <a href="http://www.cumulus4j.org/1.0.0/documentation/deployment-scenarios.html">Deployment scenarios</a>).
  * </p><p>
  * An application server using Cumulus4j is only able to read or write data, when the key manager grants access to
  * keys. In order to control this access, crypto-sessions are used (not to be confused with a servlet's session):
