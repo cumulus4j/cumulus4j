47e5db37828d73247a9cfbc47dc1ec6a4dbb83e8
Jan Mortensen <jan@nightlabs.de>
2011-10-13 12:13:42 +0000

    Former-commit-id: 607db8957d129f4fcfc94a754e9bb14f4cc54bb9

---
+++ org.cumulus4j.poleposition/src/test/java/org/poleposition/test/RunTest.java
@@ -0,0 +1,15 @@
+package org.poleposition.test;
+
+import org.junit.Test;
+import org.polepos.RunSeasonAfterEnhancing;
+
+
+public class RunTest {
+
+	 @Test
+	 public void testRun() throws Throwable{
+
+		 RunSeasonAfterEnhancing.main(null);
+	 }
+
+}
