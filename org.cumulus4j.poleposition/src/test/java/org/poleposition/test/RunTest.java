49c52b1dfae5aedd579d5cb3a62104c1d64d0f7a
Jan Mortensen <jan@nightlabs.de>
2011-10-14 13:43:46 +0000

    re insert of poleposition
    Former-commit-id: 0451992702edc149a07b0f41f069e5be7defea49

---
+++ org.cumulus4j.poleposition/src/test/java/org/poleposition/test/RunTest.java
@@ -0,0 +1,16 @@
+package org.poleposition.test;
+
+import org.junit.Test;
+import org.polepos.RunSeason;
+
+
+public class RunTest {
+
+	 @Test
+	 public void testRun() throws Throwable{
+
+//		 RunSeasonAfterEnhancing.main(null);
+		 RunSeason.main(null);
+	 }
+
+}
