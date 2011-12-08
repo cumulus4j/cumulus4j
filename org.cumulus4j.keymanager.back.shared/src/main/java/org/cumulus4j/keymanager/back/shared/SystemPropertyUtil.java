061e42613b519a8c8dd6bf690f6f2b00b9b16c12
Jan Morlock <jan.morlock.ax.ag@googlemail.com>
2011-12-08 15:13:35 +0000

    Javadoc replacement example was incorrect, now fixed. Property replacement function did not work on windows systems because of the backslash in combination with replaceAll. Fixed by using "replace" instead of "replaceAll" which should IMHO meet all requirements. Added a few test cases for this stuff.
    Former-commit-id: c673bf68a44ff65069ce6f690dfade59bc403504

--- org.cumulus4j.keymanager.back.shared/src/main/java/org/cumulus4j/keymanager/back/shared/SystemPropertyUtil.java
+++ org.cumulus4j.keymanager.back.shared/src/main/java/org/cumulus4j/keymanager/back/shared/SystemPropertyUtil.java
@@ -18,36 +18,67 @@
 package org.cumulus4j.keymanager.back.shared;
 
 import java.util.Map;
-import java.util.regex.Pattern;
+import java.util.Properties;
 
 /**
  * Helper class to work with system properties.
- *
+ * 
  * @author Marco หงุ่ยตระกูล-Schulze - marco at nightlabs dot de
  */
-public final class SystemPropertyUtil
-{
-	private SystemPropertyUtil() { }
+public final class SystemPropertyUtil {
+
+	private static final int MAX_REPLACE_ITERATIONS = 10000;
+
+	private SystemPropertyUtil() {
+	}
 
 	/**
-	 * Resolve system properties like "&#36;{java.io.tmpdir}" in a <code>String</code>.
-	 * Note, that this method works recursively, i.e. you can specify a system property
-	 * "<code>aaa = someString/&#36;{bbb}/other</code>" and a system property "<code>bbb = xxx</code>" and the string
-	 * "<code>yyy/&#36;{aaa}/zzz</code>" will be resolved to "<code>yyy/someString/xxx/other</code>".
-	 *
-	 * @param raw the raw <code>String</code>, e.g. "&#36;{java.io.tmpdir}/mydirectory".
+	 * Resolve system properties like "&#36;{java.io.tmpdir}" in a
+	 * <code>String</code>. Note, that this method works recursively, i.e. you
+	 * can specify a system property "
+	 * <code>aaa = someString/&#36;{bbb}/other</code>" and a system property "
+	 * <code>bbb = xxx</code>" and the string "<code>yyy/&#36;{aaa}/zzz</code>
+	 * " will be resolved to "<code>yyy/someString/xxx/other/zzz</code>".
+	 * 
+	 * @param raw
+	 *            the raw <code>String</code>, e.g.
+	 *            "&#36;{java.io.tmpdir}/mydirectory".
 	 * @return the resolved <code>String</code>, e.g. "/tmp/mydirectory".
 	 */
-	public static String resolveSystemProperties(String raw)
-	{
+	public static String resolveSystemProperties(String raw) {
+		return resolveProperties(raw, System.getProperties());
+	}
+
+	/**
+	 * General core of <code>resolveSystemProperties</code> simplifying the
+	 * creation of test functions.
+	 * 
+	 * @param raw
+	 *            the raw <code>String</code>, e.g.
+	 *            "&#36;{java.io.tmpdir}/mydirectory".
+	 * @param properties
+	 *            Properties to resolve
+	 * @return the resolved <code>String</code>, e.g. "/tmp/mydirectory".
+	 */
+	public static String resolveProperties(String raw, Properties properties) {
 		String result = raw;
-		while (result.indexOf('$') >= 0) {
-			for (Map.Entry<?, ?> sysProp : System.getProperties().entrySet()) {
-				result = result.replaceAll(
-						"\\$\\{" + Pattern.quote(String.valueOf(sysProp.getKey())) + "\\}", String.valueOf(sysProp.getValue())
-				);
+
+		// Counter to prevent running forever if an invalid string was
+		// specified.
+		for (int i = 0; (result.indexOf('$') >= 0)
+				&& (i < MAX_REPLACE_ITERATIONS); i++) {
+			for (Map.Entry<?, ?> prop : properties.entrySet()) {
+				String target = "${" + String.valueOf(prop.getKey()) + "}";
+				result = result
+						.replace(target, String.valueOf(prop.getValue()));
 			}
 		}
+
+		if (result.indexOf('$') >= 0) {
+			throw new IllegalArgumentException(
+					"Property replacement failed for string " + raw);
+		}
+
 		return result;
 	}
 }
