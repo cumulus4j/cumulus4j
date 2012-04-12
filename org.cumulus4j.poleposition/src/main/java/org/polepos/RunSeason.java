28c3854196181fb5629d73c637762547d6f82a4e
Jan Mortensen <jan@nightlabs.de>
2012-04-12 09:28:45 +0000

    Former-commit-id: 37c5a72687962eb7909a2d941ef382e6c513eab0

--- org.cumulus4j.poleposition/src/main/java/org/polepos/RunSeason.java
+++ org.cumulus4j.poleposition/src/main/java/org/polepos/RunSeason.java
@@ -99,8 +99,8 @@ public class RunSeason extends AbstractRunner {
 //				 new JviTeam(),
 //				 new CobraTeam(),
 
-				new JdoCumulus4jTeam(runtimeProperties),
-				new JdoTeam()
+				new JdoTeam(),
+				new JdoCumulus4jTeam(runtimeProperties)
 		};
 	}
 
