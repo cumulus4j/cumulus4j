9e89aa3834315a901283a2c60ffb84647fdd4112
Jan Mortensen <jan@nightlabs.de>
2012-06-27 10:35:09 +0000

    Added second data source (and a test for it).
    Former-commit-id: ff207a40c0dbeea2bf7397c37b16ae0f6271ac42

---
+++ org.cumulus4j.jee.test/org.cumulus4j.jee.test.glassfish/src/test/java/org/cumulus4j/jee/test/glassfish/DefaultDataSourceC4jEjbInvocationIT.java
@@ -0,0 +1,42 @@
+package org.cumulus4j.jee.test.glassfish;
+
+import javax.naming.InitialContext;
+
+import org.cumulus4j.jee.test.ejb.DefaultDataSourceC4jTestRemote;
+import org.junit.Test;
+
+public class DefaultDataSourceC4jEjbInvocationIT extends AbstractGlassfishIT {
+
+	@Test
+	public void defaultDataSourceC4jEjbInvocation() throws Exception {
+		System.out.println("defaultDataSourceC4jEjbInvocation: Entered.");
+		boolean successful = false;
+		int tryCounter = 0;
+		while (!successful) {
+			++tryCounter;
+			try {
+				InitialContext ic = createInitialContext();
+
+				System.out.println("defaultDataSourceC4jEjbInvocation: Created InitialContext instance. Looking up EJB.");
+
+				DefaultDataSourceC4jTestRemote remote = (DefaultDataSourceC4jTestRemote)ic.lookup(DefaultDataSourceC4jTestRemote.class.getName());
+
+				System.out.println("defaultDataSourceC4jEjbInvocation: Looked up EJB. Invoking remote method.");
+
+				remote.test();
+
+				System.out.println("defaultDataSourceC4jEjbInvocation: Invoked remote method successfully.");
+				successful = true;
+			} catch (Exception x) {
+				if (tryCounter >= 3) // We try it 3 times - if it fails for the 3rd time, we rethrow.
+					throw x;
+
+				System.out.println("defaultDataSourceC4jEjbInvocation: Caught exception! Will retry.");
+				x.printStackTrace();
+				System.out.println("defaultDataSourceC4jEjbInvocation: Sleeping...");
+				Thread.sleep(5000);
+			}
+		}
+	}
+
+}
